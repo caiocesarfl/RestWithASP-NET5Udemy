8b88b944835fd6b9f4d3e61cb9da6dd0fb9473a3
Caio Lara <caio.lara@dtidigital.com.br>
2021-02-12 22:41:37 -0300

    Implementing a simple API with four verbs GET,POST,PUT and Delete to training implement API with .NET

---
+++ RestWithASP-NETUdemy/CalculatorWithASP-NETUdemy/RestWithASP-NETUdemy/Services/Implementations/PersonServiceImplementations.cs
@@ -0,0 +1,66 @@
+﻿using RestWithASP_NETUdemy.Model;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestWithASP_NETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementations : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+          
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i=0 ; i<8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Caio",
+                LastName = "Lara",
+                Address = "Rua Dr Jaci de Morais Lima",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person Last Name +" + i,
+                Address = "Person Address" + i,
+                Gender = "Male"
+            }; ;
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment (ref count);
+        }
+    }
+}
--- RestWithASP-NETUdemy/CalculatorWithASP-NETUdemy/RestWithASP-NETUdemy/Startup.cs
+++ RestWithASP-NETUdemy/CalculatorWithASP-NETUdemy/RestWithASP-NETUdemy/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithASP_NETUdemy.Services;
+using RestWithASP_NETUdemy.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +29,7 @@ namespace RestWithASP_NETUdemy
         {
 
             services.AddControllers();
+            services.AddScoped<IPersonService, PersonServiceImplementations>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
