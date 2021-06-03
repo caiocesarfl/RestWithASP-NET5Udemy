ddb6e89303b31052339f30c79419e6d12e78c945
Caio Lara <caio.freitas.ita@gmail.com>
2021-06-03 17:24:58 -0300

    Implement Search by Fisrt name with Paged Search on Repository And Business

--- RestWithASP-NETUdemy/RestWithASP-NETUdemy/RestWithASP-NETUdemy/Repository/Generic/GenericRepository.cs
+++ RestWithASP-NETUdemy/RestWithASP-NETUdemy/RestWithASP-NETUdemy/Repository/Generic/GenericRepository.cs
@@ -93,5 +93,24 @@ namespace RestWithASP_NETUdemy.Repository.Generic
             return dataset.Any(p => p.Id.Equals(id));
         }
 
+        public List<T> FindWithPagedSearch(string query)
+        {
+            return dataset.FromSqlRaw<T>(query).ToList();
+        }
+
+        public int GetCount(string query)
+        {
+            var result = "";
+            using (var connection = _context.Database.GetDbConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    result = command.ExecuteScalar().ToString();
+                }
+            }
+            return int.Parse(result);
+        }
     }
 }
