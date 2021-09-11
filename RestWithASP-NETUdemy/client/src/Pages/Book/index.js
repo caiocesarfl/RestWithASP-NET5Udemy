84a2e376d34c5f384ab0489156d92a077329e42b
Caio Lara <caio.lara@dtidigital.com.br>
2021-09-11 14:48:59 -0300

    Implement grid of book on book screen

--- RestWithASP-NETUdemy/client/src/Pages/Book/index.js
+++ RestWithASP-NETUdemy/client/src/Pages/Book/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
@@ -16,6 +16,85 @@ export default function Book () {
                     <FiPower size={18} color='#251FC5'></FiPower>
                 </button>
             </header>
+            <h1>Registred Books</h1>
+            <ul>
+                <li>
+                    <strong>Title: </strong>
+                    <p>Seja Foda</p>
+                    <strong>Author: </strong>
+                    <p>Caio Carneiro</p>
+                    <strong>Price: </strong>
+                    <p>R$ 25,00</p>
+                    <strong>Release Date: </strong>
+                    <p>25/10/2017</p>
+
+                    <button type="button"> 
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type= "button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title: </strong>
+                    <p>Seja Foda</p>
+                    <strong>Author: </strong>
+                    <p>Caio Carneiro</p>
+                    <strong>Price: </strong>
+                    <p>R$ 25,00</p>
+                    <strong>Release Date: </strong>
+                    <p>25/10/2017</p>
+
+                    <button type="button"> 
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type= "button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title: </strong>
+                    <p>Seja Foda</p>
+                    <strong>Author: </strong>
+                    <p>Caio Carneiro</p>
+                    <strong>Price: </strong>
+                    <p>R$ 25,00</p>
+                    <strong>Release Date: </strong>
+                    <p>25/10/2017</p>
+
+                    <button type="button"> 
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type= "button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title: </strong>
+                    <p>Seja Foda</p>
+                    <strong>Author: </strong>
+                    <p>Caio Carneiro</p>
+                    <strong>Price: </strong>
+                    <p>R$ 25,00</p>
+                    <strong>Release Date: </strong>
+                    <p>25/10/2017</p>
+
+                    <button type="button"> 
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type= "button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
+
         </div>
     );
 }
