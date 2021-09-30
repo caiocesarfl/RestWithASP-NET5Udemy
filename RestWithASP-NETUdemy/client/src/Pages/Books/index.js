a32fddb15cee05325f353a6368772d773dc0c2ee
Caio Lara <caio.lara@dtidigital.com.br>
2021-09-29 23:07:16 -0300

    Integrate book list screen with api

--- RestWithASP-NETUdemy/client/src/Pages/Books/index.js
+++ RestWithASP-NETUdemy/client/src/Pages/Books/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
+
+import api from '../../services/api'
 
 import logoImage from '../../assets/logo.svg';
 
 export default function Books () {
+    const [books, setBooks] = useState([]);
+
+    const userName = localStorage.getItem('userName');
+
+    const acessToken = localStorage.getItem('acessToken');
+
+    const history = useHistory();
+
+    useEffect(() => {
+        api.get('api/Book/v1/asc/5/1', {
+            headers: {
+                Authorization: `Bearer ${acessToken}`
+            }   
+        }).then((response) => {
+            setBooks(response.data)
+        })
+    },[acessToken]);
+ 
+    console.log (books);
     return (
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="Caioba logo"/>
-                <span>Welcome, <strong>Ravi</strong></span>
+                <span>Welcome, <strong>{userName.toUpperCase()}</strong></span>
                 <Link className="button" to="book/new"> Add New Book </Link>
                 <button type="button">
                     <FiPower size={18} color='#251FC5'></FiPower>
@@ -18,83 +39,28 @@ export default function Books () {
             </header>
             <h1>Registred Books</h1>
             <ul>
-                <li>
-                    <strong>Title: </strong>
-                    <p>Seja Foda</p>
-                    <strong>Author: </strong>
-                    <p>Caio Carneiro</p>
-                    <strong>Price: </strong>
-                    <p>R$ 25,00</p>
-                    <strong>Release Date: </strong>
-                    <p>25/10/2017</p>
-
-                    <button type="button"> 
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-
-                    <button type= "button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Title: </strong>
-                    <p>Seja Foda</p>
-                    <strong>Author: </strong>
-                    <p>Caio Carneiro</p>
-                    <strong>Price: </strong>
-                    <p>R$ 25,00</p>
-                    <strong>Release Date: </strong>
-                    <p>25/10/2017</p>
-
-                    <button type="button"> 
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-
-                    <button type= "button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Title: </strong>
-                    <p>Seja Foda</p>
-                    <strong>Author: </strong>
-                    <p>Caio Carneiro</p>
-                    <strong>Price: </strong>
-                    <p>R$ 25,00</p>
-                    <strong>Release Date: </strong>
-                    <p>25/10/2017</p>
-
-                    <button type="button"> 
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-
-                    <button type= "button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Title: </strong>
-                    <p>Seja Foda</p>
-                    <strong>Author: </strong>
-                    <p>Caio Carneiro</p>
-                    <strong>Price: </strong>
-                    <p>R$ 25,00</p>
-                    <strong>Release Date: </strong>
-                    <p>25/10/2017</p>
-
-                    <button type="button"> 
-                        <FiEdit size={20} color="#251FC5"/>
-                    </button>
-
-                    <button type= "button">
-                        <FiTrash2 size={20} color="#251FC5"/>
-                    </button>
-                </li>
+                
+                    {books.map(book => {
+                        <li>
+                               <strong>Title: </strong>
+                               <p>{book.title}</p>
+                               <strong>Author: </strong>
+                               <p>{book.author}</p>
+                               <strong>Price: </strong>
+                               <p>{book.price}</p>
+                               <strong>Release Date: </strong>
+                               <p>{book.lauchDate}</p>
+           
+                               <button type="button"> 
+                                   <FiEdit size={20} color="#251FC5"/>
+                               </button>
+           
+                               <button type= "button">
+                                   <FiTrash2 size={20} color="#251FC5"/>
+                               </button>  
+                        </li>
+                    })}
             </ul>
-
         </div>
     );
 }
